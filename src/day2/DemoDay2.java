7493bb8b181a4b716bb1b354d0bda9708027c267
danil <danil@LAPTOP-CUC34H8F>
2023-08-24 09:14:19 -0400

    updated

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -4,5 +4,14 @@ public class DemoDay2 {
 
 	public static void main(String[] args) {
 		System.out.println("Test");
+		
+	}
+	
+	public static void add(int a, int b) {
+		System.out.println(a+b);
+	}
+	
+	public static void subtract(int c, int d) {
+		System.out.println(c-d);
 	}
 }
