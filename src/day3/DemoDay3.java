33e309a1411c42ca4cbf9ab16584fa2031999132
Lucheria <140756588+Lucheria@users.noreply.github.com>
2023-08-24 10:14:14 -0400

    Update DemoDay3.java

--- src/day3/DemoDay3.java
+++ src/day3/DemoDay3.java
@@ -2,9 +2,10 @@ package day3;
 
 public class DemoDay3 {
 public static void main(String[] args) {
-	System.out.println("Lucheria");
-
 	String country = "USA";
 	String city = "MIAMI";
+	System.out.println("Lucheria");
+
+	
 }
 }
